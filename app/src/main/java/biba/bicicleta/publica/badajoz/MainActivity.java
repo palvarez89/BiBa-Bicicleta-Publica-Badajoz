6e4294ee887abd7f84c547816f4894e28ee95d4a
Pedro Alvarez <palvarez89@gmail.com>
2015-04-11 14:19:12 +0100

    Import swipe-to-refresh demo

---
+++ app/src/main/java/biba/bicicleta/publica/badajoz/MainActivity.java
@@ -0,0 +1,48 @@
+package biba.bicicleta.publica.badajoz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+
+public class MainActivity extends Activity {
+
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private RecyclerView mRecyclerView;
+    private CatNamesRecyclerViewAdapter mCatNamesRecyclerViewAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.activity_main_swipe_refresh_layout);
+        mRecyclerView = (RecyclerView) findViewById(R.id.activity_main_recyclerview);
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        setupAdapter();
+
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.orange, R.color.green, R.color.blue);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        setupAdapter();
+                        mSwipeRefreshLayout.setRefreshing(false);
+                    }
+                }, 2500);
+            }
+        });
+    }
+
+    private void setupAdapter() {
+        mCatNamesRecyclerViewAdapter = new CatNamesRecyclerViewAdapter(this);
+        mRecyclerView.setAdapter(mCatNamesRecyclerViewAdapter);
+    }
+
+}
