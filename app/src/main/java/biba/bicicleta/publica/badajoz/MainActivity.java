3a4c48f610f45607c7e423a43df9510dab7d8fad
Pedro Alvarez <palvarez89@gmail.com>
2015-04-12 11:54:19 +0100

    swipe to refresh "works", but doesn't refresh

--- app/src/main/java/biba/bicicleta/publica/badajoz/MainActivity.java
+++ app/src/main/java/biba/bicicleta/publica/badajoz/MainActivity.java
@@ -1,74 +1,154 @@
 package biba.bicicleta.publica.badajoz;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONException;
 
 import java.util.Vector;
 
-import biba.bicicleta.publica.badajoz.fragments.ActivityCommunicator;
-import biba.bicicleta.publica.badajoz.fragments.ListaEstaciones;
+import biba.bicicleta.publica.badajoz2.R;
+
+//import biba.bicicleta.publica.badajoz.fragments.ActivityCommunicator;
+//import biba.bicicleta.publica.badajoz.fragments.ListaEstaciones;
 
 
-public class MainActivity extends FragmentActivity implements
-        ActivityCommunicator {
+public class MainActivity extends Activity {
+
+    Vector<Estacion> estaciones;
+    InfoEstaciones infoEstaciones;
+
+    LoadJson asyncTask;
+
+    Activity activity;
+
+    MiAdaptador adaptador;
 
     String deb = "DEBUG";
     Fragment listFragment = null;
     boolean enFavs = false;
 
-    private SwipeRefreshLayout mSwipeRefreshLayout;
-    private RecyclerView mRecyclerView;
-    private CatNamesRecyclerViewAdapter mCatNamesRecyclerViewAdapter;
+    private GeneralSwipeRefreshLayout mSwipeRefreshLayout;
+//    private RecyclerView mRecyclerView;
+//    private CatNamesRecyclerViewAdapter mCatNamesRecyclerViewAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        activity = this;
 
-        listFragment = new ListaEstaciones(enFavs);
+        estaciones = new Vector<Estacion>();
+        infoEstaciones = InfoEstaciones.getInstance();
 
-        listFragment.setRetainInstance(true);
-        FragmentTransaction transList = getSupportFragmentManager().beginTransaction();
-        transList.add(R.id.listaestaciones_f_container, listFragment);
-        transList.commit();
+        final ListView listView;
+        listView = (ListView) findViewById(R.id.list);
 
-        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.activity_main_swipe_refresh_layout);
+        new LoadJson(activity, listView).execute();
+
+        // Defined Array values to show in ListView
+        String[] values = new String[] {
+                "Android List View",
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android",
+                "Android Example",
+                "List View Source Code",
+                "List View Array Adapter",
+                "Android List View",
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android",
+                "Android Example",
+                "List View Source Code",
+                "List View Array Adapter",
+                "Android List View",
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android",
+                "Android Example",
+                "List View Source Code",
+                "List View Array Adapter",
+                "Android Example List View"
+        };
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+//
+//        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+
+        /**************** Create Custom Adapter *********/
+
+//        listView.setBackgroundColor(0xFF888388);
+
+//        listFragment = new ListaEstaciones(enFavs);
+//
+//        listFragment.setRetainInstance(true);
+//        FragmentTransaction transList = getSupportFragmentManager().beginTransaction();
+//        transList.add(R.id.listaestaciones_f_container, listFragment);
+//        transList.commit();
+
+        mSwipeRefreshLayout = (GeneralSwipeRefreshLayout) findViewById(R.id.activity_main_swipe_refresh_layout);
+
 //        mRecyclerView = (RecyclerView) findViewById(R.id.activity_main_recyclerview);
 //        final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
 //        mRecyclerView.setLayoutManager(layoutManager);
 //        setupAdapter();
 
         mSwipeRefreshLayout.setColorSchemeResources(R.color.orange, R.color.green, R.color.blue);
-        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+
+        mSwipeRefreshLayout.setOnChildScrollUpListener(new GeneralSwipeRefreshLayout.OnChildScrollUpListener() {
+            @Override
+            public boolean canChildScrollUp() {
+                return listView.getFirstVisiblePosition() > 0 ||
+                        listView.getChildAt(0) == null ||
+                        listView.getChildAt(0).getTop() < 0;
+            }
+        });
+
+        mSwipeRefreshLayout.setOnRefreshListener(new GeneralSwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
 //                        setupAdapter();
-                        update();
-                        mSwipeRefreshLayout.setRefreshing(false);
+//                        update();
+
+//                        This is not possible, use AsyncTask
+//                        try {
+//                            wait(2000);
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
+                        new LoadJson(activity, listView).execute();
+
                     }
                     // TODO: this should wait until refresh
                 }, 2);
             }
         });
     }
-
-    private void setupAdapter() {
-        mCatNamesRecyclerViewAdapter = new CatNamesRecyclerViewAdapter(this);
-        mRecyclerView.setAdapter(mCatNamesRecyclerViewAdapter);
-    }
-
 
     public void passDataToActivity(Vector<Estacion> estaciones) {
         Log.w(deb, "BiBa Activity passDataToActivity");
@@ -101,7 +181,7 @@ public class MainActivity extends FragmentActivity implements
         // listFragment.setRetainInstance(true);
         // }
 
-        ((ListaEstaciones) listFragment).update();
+//        ((ListaEstaciones) listFragment).update();
         // FragmentTransaction transaction = getSupportFragmentManager()
         // .beginTransaction();
         // transaction.detach(listFragment);
@@ -109,4 +189,52 @@ public class MainActivity extends FragmentActivity implements
         // transaction.commit();
         Log.w(deb, "BiBa Activity update ");
     }
+
+
+    public class LoadJson extends AsyncTask<Void, Integer, Vector<Estacion>> {
+
+        Activity activity;
+        ListView listView;
+        GeneralSwipeRefreshLayout swipeLayout;
+
+        public LoadJson(Activity activity, ListView listView){
+            this.activity = activity;
+            this.listView = listView;
+            swipeLayout = (GeneralSwipeRefreshLayout) activity.findViewById(R.id.activity_main_swipe_refresh_layout);
+
+        }
+
+        @Override
+        protected Vector<Estacion> doInBackground(Void... params) {
+
+
+            try {
+                estaciones = new Vector<Estacion>();
+                estaciones = infoEstaciones.getInfo(false);
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return estaciones;
+        }
+
+        @Override
+        protected void onPostExecute(Vector<Estacion> result) {
+
+
+            adaptador = new MiAdaptador(activity, result);
+            adaptador.notifyDataSetChanged();
+            listView.setAdapter(adaptador);
+
+            swipeLayout.setRefreshing(false);
+
+        }
+    }
+
+
+    public interface AsyncResponse {
+        void updateList(Vector<Estacion> result);
+    }
 }
+
+
