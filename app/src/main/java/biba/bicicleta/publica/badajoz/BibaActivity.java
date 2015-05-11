d0f6c8548114cf31794e052f3d6d024f8ea47e06
Pedro Alvarez <palvarez89@gmail.com>
2015-05-12 00:05:24 +0100

    BibaActivity: Add application drawer

--- app/src/main/java/biba/bicicleta/publica/badajoz/BibaActivity.java
+++ app/src/main/java/biba/bicicleta/publica/badajoz/BibaActivity.java
@@ -1,9 +1,12 @@
 package biba.bicicleta.publica.badajoz;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 
 import biba.bicicleta.publica.badajoz.fragments.ListaEstaciones;
@@ -14,6 +17,7 @@ public class BibaActivity extends ActionBarActivity {
 
     Analytics analytics;
     Fragment listFragment;
+    ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,15 @@ public class BibaActivity extends ActionBarActivity {
         // Set a toolbar to replace the action bar.
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
 
         if (savedInstanceState != null) {
 
@@ -51,6 +64,19 @@ public class BibaActivity extends ActionBarActivity {
                 .beginTransaction();
         transList.add(R.id.listaestaciones_f_container, listFragment);
         transList.commit();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
 
