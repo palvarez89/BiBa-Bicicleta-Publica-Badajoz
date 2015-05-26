1ec4f27ea5514b40e39247005ca04406adbd037c
Pedro Alvarez <palvarez89@gmail.com>
2015-05-27 00:35:12 +0100

    Map: Implement Map fragment, with markers
    Issues:
    
    - On rotation, if a Marker was open, it's closed.

--- app/src/main/java/biba/bicicleta/publica/badajoz/fragments/Map.java
+++ app/src/main/java/biba/bicicleta/publica/badajoz/fragments/Map.java
@@ -1,49 +1,77 @@
 package biba.bicicleta.publica.badajoz.fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.octo.android.robospice.JacksonSpringAndroidSpiceService;
+import com.octo.android.robospice.SpiceManager;
+import com.octo.android.robospice.persistence.DurationInMillis;
+import com.octo.android.robospice.persistence.exception.SpiceException;
+import com.octo.android.robospice.request.listener.RequestListener;
 
+import biba.bicicleta.publica.badajoz.BibaApp;
 import biba.bicicleta.publica.badajoz.R;
+import biba.bicicleta.publica.badajoz.objects.Estacion2;
+import biba.bicicleta.publica.badajoz.utils.EstacionList;
+import biba.bicicleta.publica.badajoz.utils.StationsRequest;
 
 public class Map extends Fragment {
 
-    static final LatLng HAMBURG = new LatLng(53.558, 9.927);
-    static final LatLng KIEL = new LatLng(53.551, 9.993);
-    private GoogleMap map;
+    private GoogleMap map = null;
+    private Activity activity;
+    private CameraPosition camerePosition;
+    BibaApp bibaApp;
+    protected SpiceManager spiceManager = new SpiceManager(JacksonSpringAndroidSpiceService.class);
+
+
+    @Override
+    public void onStart() {
+        bibaApp = (BibaApp) activity.getApplicationContext();
+        super.onStart();
+        spiceManager.start(activity);
+        performRequest(false);
+    }
+
+    @Override
+    public void onStop() {
+        if (spiceManager.isStarted()) {
+            spiceManager.shouldStop();
+        }
+        super.onStop();
+    }
+
+    private void performRequest(boolean force) {
+        if (!force && bibaApp.estaciones != null) {
+            updateMap(bibaApp.estaciones);
+            return;
+        }
+        StationsRequest request = new StationsRequest();
+        spiceManager.execute(request, "cache", DurationInMillis.ONE_MINUTE, new EstacionListRequestListener());
+    }
+
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        setUpMapIfNeeded();
+    }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        map = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map))
-                .getMap();
-        Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
-                .title("Hamburg"));
-        Marker kiel = map.addMarker(new MarkerOptions()
-                .position(KIEL)
-                .title("Kiel")
-                .snippet("Kiel is cool")
-                .icon(BitmapDescriptorFactory
-                        .fromResource(R.drawable.bike)));
-
-        // Move the camera instantly to hamburg with a zoom of 15.
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
-
-        // Zoom in, animating the camera.
-        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+        setRetainInstance(true);
     }
 
     @Override
@@ -51,4 +79,83 @@ public class Map extends Fragment {
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_map, container, false);
     }
+
+    private void setUpMapIfNeeded() {
+        if (map == null) {
+            map = ((SupportMapFragment) getChildFragmentManager()
+                    .findFragmentById(R.id.map)).getMap();
+            if (map != null) {
+                map.setMyLocationEnabled(true);
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(
+                        38.874463, -6.974258), 12.0f));
+                GoogleMapOptions options = new GoogleMapOptions();
+                options.mapType(GoogleMap.MAP_TYPE_TERRAIN)
+                        .compassEnabled(false).rotateGesturesEnabled(false)
+                        .tiltGesturesEnabled(false).zoomControlsEnabled(true);
+                performRequest(true);
+            }
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        this.activity = activity;
+    }
+
+    public void onPause() {
+        super.onPause();
+        if (map != null) {
+            camerePosition = map.getCameraPosition();
+            map = null;
+        }
+    }
+
+    public void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+        if (camerePosition != null) {
+            map.moveCamera(CameraUpdateFactory.newCameraPosition(camerePosition));
+            camerePosition = null;
+        }
+    }
+
+    private void showPoint(Estacion2 station) {
+        if (map != null) {
+            map.addMarker(new MarkerOptions()
+                    .position(
+                            new LatLng(station.getLat(), station
+                                    .getLon()))
+                    .title(station.getName())
+                    .snippet(
+                            "Bicis: " + station.getAvail()
+                                    + " Parkings: " + station.getSpace()));
+        }
+    }
+
+    public void updateMap(EstacionList estaciones) {
+        if (isAdded()) {
+            if (estaciones != null) {
+                for (int i = 0; i < estaciones.size(); i++) {
+                    showPoint(estaciones.get(i));
+                }
+            }
+        }
+    }
+
+    private class EstacionListRequestListener implements RequestListener<EstacionList> {
+        @Override
+        public void onRequestFailure(SpiceException spiceException) {
+            Toast.makeText(activity.getApplicationContext(), R.string.failed_update,
+                    Toast.LENGTH_LONG).show();
+            updateMap(null);
+        }
+
+        @Override
+        public void onRequestSuccess(EstacionList estaciones) {
+            bibaApp.updateEstaciones(estaciones);
+            updateMap(estaciones);
+        }
+    }
+
 }
