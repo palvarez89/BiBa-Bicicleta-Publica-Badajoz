edc180a5e71a3db5cdf9ceb64b9acc114f6a47d5
Pedro Alvarez <palvarez89@gmail.com>
2016-08-25 02:00:26 +0200

    Create new activity for Estaciones details

---
+++ app/src/main/java/biba/bicicleta/publica/badajoz/EstacionDetallesActivity.java
@@ -0,0 +1,28 @@
+package biba.bicicleta.publica.badajoz;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class EstacionDetallesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estacion_detalles);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
