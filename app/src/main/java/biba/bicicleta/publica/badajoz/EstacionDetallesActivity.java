959976892cf405584436695333ef445d97a9eaf5
Pedro Alvarez <palvarez89@gmail.com>
2016-08-27 16:31:41 +0200

    EstacionDetalles: Add header card

--- app/src/main/java/biba/bicicleta/publica/badajoz/EstacionDetallesActivity.java
+++ app/src/main/java/biba/bicicleta/publica/badajoz/EstacionDetallesActivity.java
@@ -1,13 +1,19 @@
 package biba.bicicleta.publica.badajoz;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.octo.android.robospice.Jackson2SpringAndroidSpiceService;
@@ -17,9 +23,12 @@ import com.octo.android.robospice.persistence.exception.SpiceException;
 import com.octo.android.robospice.request.listener.RequestListener;
 
 import biba.bicicleta.publica.badajoz.adapters.EstacionDetallesAdapter;
+import biba.bicicleta.publica.badajoz.adapters.ListaEstacionesAdapter;
+import biba.bicicleta.publica.badajoz.objects.Estacion;
 import biba.bicicleta.publica.badajoz.objects.MessageList;
 import biba.bicicleta.publica.badajoz.utils.CommentsRequest;
 import biba.bicicleta.publica.badajoz.utils.GeneralSwipeRefreshLayout;
+import biba.bicicleta.publica.badajoz.views.EstacionViewHolder;
 
 public class EstacionDetallesActivity extends AppCompatActivity {
 
@@ -27,12 +36,14 @@ public class EstacionDetallesActivity extends AppCompatActivity {
     private GeneralSwipeRefreshLayout swipeLayout;
     private final SpiceManager spiceManager = new SpiceManager(Jackson2SpringAndroidSpiceService.class);
     private int stationNumber;
+    private CardView topCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        stationNumber = Integer.valueOf(getIntent().getExtras().getString("estacion"));
+        Estacion e = (Estacion) getIntent().getParcelableExtra("estacion");
+        stationNumber = e.getN();
 
         setContentView(R.layout.activity_estacion_detalles);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -61,6 +72,9 @@ public class EstacionDetallesActivity extends AppCompatActivity {
 //            }
 //        });
 
+        initTopCard(e);
+
+
         messagesRecycler = (RecyclerView) findViewById(R.id.messageList);
         messagesRecycler.setHasFixedSize(true);
         LinearLayoutManager llm = new LinearLayoutManager(this);
@@ -75,6 +89,62 @@ public class EstacionDetallesActivity extends AppCompatActivity {
         performRequest(false);
     }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (swipeLayout != null) {
+            swipeLayout.setRefreshing(false);
+            swipeLayout.destroyDrawingCache();
+            swipeLayout.clearAnimation();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        if (spiceManager.isStarted()) {
+            spiceManager.shouldStop();
+        }
+        super.onStop();
+    }
+
+    private void initTopCard(Estacion e) {
+        topCardView = (CardView) findViewById(R.id.top_card_detalles);
+
+        topCardView.setRadius(0);
+        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams)topCardView.getLayoutParams();
+        params.setMargins(0, 0, 0, 0); //substitute parameters for left, top, right, bottom
+        topCardView.setLayoutParams(params);
+        Resources r = getResources();
+        float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2, r.getDisplayMetrics());
+        topCardView.setCardElevation(px);
+
+        final EstacionViewHolder evh = EstacionViewHolder.newInstance(topCardView);
+        final EstacionViewHolder evhFinal = evh;
+        int numero = e.getN();
+        String nombre = toLowerCase(e.getName());
+        int bicis = e.getAvail();
+        int parkings = e.getSpace();
+        boolean estado = e.getStateBool();
+        evh.setEstacionInfo(numero, nombre, bicis, parkings, estado);
+
+        final SharedPreferences prefs;
+        prefs = getSharedPreferences(
+                "biba.bicicleta.publica.badajoz", Context.MODE_PRIVATE);
+        final int realPosition = numero -1;
+        final boolean isFav = prefs.getBoolean("fav" + realPosition, false);
+
+        evh.setFavStar(isFav);
+
+        evh.mFavStar.setOnClickListener(new CardClickListener(isFav) {
+            @Override
+            public void onClick(View v) {
+                isFav = !isFav;
+                evhFinal.setFavStar(isFav);
+                prefs.edit().putBoolean("fav" + realPosition, isFav).commit();
+
+            }
+        });
+    }
     private void initSwipeLayout() {
         swipeLayout = (GeneralSwipeRefreshLayout) findViewById(
                 R.id.activity_estacion_detalles_swipe_refresh_layout);
@@ -147,4 +217,39 @@ public class EstacionDetallesActivity extends AppCompatActivity {
             messagesRecycler.setAdapter(eda);
         }
     }
+
+
+
+    private String toLowerCase(String str) {
+        String[] words = str.split("\\s");
+        String out = "";
+
+        for (int i = 0; i < words.length - 1; i++) {
+            out = out + toLowerCaseWord(words[i]) + " ";
+        }
+        out = out + toLowerCaseWord(words[words.length - 1]);
+        return out;
+    }
+
+    private String toLowerCaseWord(String str) {
+
+        if (str.length() == 0) return "";
+
+        if (str.length() == 1) return str.toUpperCase();
+
+        if (!Character.isLetter(str.charAt(0))) {
+            return str.substring(0, 1) + str.substring(1, 2).toUpperCase() + str.substring(2).toLowerCase();
+        } else {
+            return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+        }
+    }
+
+    public abstract class CardClickListener implements View.OnClickListener {
+        boolean isFav;
+
+        public CardClickListener(boolean isFav) {
+            this.isFav = isFav;
+        }
+    }
+
 }
