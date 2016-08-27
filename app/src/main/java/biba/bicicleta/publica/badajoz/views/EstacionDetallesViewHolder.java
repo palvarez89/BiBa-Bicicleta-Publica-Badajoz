1c8e45662b066548ed2855562cf6adeb191af885
Pedro Alvarez <palvarez89@gmail.com>
2016-08-27 21:47:53 +0200

    estacion_detalles_card styling

--- app/src/main/java/biba/bicicleta/publica/badajoz/views/EstacionDetallesViewHolder.java
+++ app/src/main/java/biba/bicicleta/publica/badajoz/views/EstacionDetallesViewHolder.java
@@ -1,24 +1,31 @@
 package biba.bicicleta.publica.badajoz.views;
 
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
 import android.view.View;
 import android.widget.TextView;
 
+import java.sql.Timestamp;
+
 import biba.bicicleta.publica.badajoz.R;
+import biba.bicicleta.publica.badajoz.objects.Message;
 
 
-public class EstacionDetallesViewHolder extends RecyclerView.ViewHolder{
+public class EstacionDetallesViewHolder extends RecyclerView.ViewHolder {
     protected TextView vMessage;
     protected TextView vTime;
 
     public EstacionDetallesViewHolder(View v) {
         super(v);
-        vMessage =  (TextView) v.findViewById(R.id.txtMessage);
-        vTime = (TextView)  v.findViewById(R.id.txtTime);
+        vMessage = (TextView) v.findViewById(R.id.txtMessage);
+        vTime = (TextView) v.findViewById(R.id.txtTime);
     }
 
-    public void setDetails(String msg, String date) {
-        vMessage.setText(msg);
+    public void setDetails(Message msg) {
+        vMessage.setText(msg.getMessage());
+        Timestamp ts = msg.getTime();
+        long currentTime = System.currentTimeMillis();
+        String date = (String) DateUtils.getRelativeTimeSpanString(ts.getTime(), currentTime, DateUtils.MINUTE_IN_MILLIS);
         vTime.setText(date);
     }
 }
