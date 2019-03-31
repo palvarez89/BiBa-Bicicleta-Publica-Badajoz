4deceaaa9805de78f8e59d20b5a95c2bef95a9a3
Pedro Alvarez <pedro.alvarez@codethink.co.uk>
2019-03-31 11:53:44 +0200

    Use scrapper in python in heroku

--- app/src/main/java/biba/bicicleta/publica/badajoz/utils/StationsRequest.java
+++ app/src/main/java/biba/bicicleta/publica/badajoz/utils/StationsRequest.java
@@ -12,9 +12,11 @@ import biba.bicicleta.publica.badajoz.objects.EstacionList;
 public class StationsRequest extends SpringAndroidSpiceRequest<EstacionList> {
     private int intento=0;
     private final String[] urls = {
-            "http://biba8.000webhostapp.com/4/getEstaciones.php", // https://www.000webhost.com/members/website/biba8/build
-            "http://biba7.000webhostapp.com/4/getEstaciones.php", // also 000webhost
-            "http://biba4.epizy.com/4/getEstaciones.php", // https://app.infinityfree.net/accounts
+            "https://bibanotes.herokuapp.com/get-estaciones",
+            // These are giving me problems
+            // "http://biba4.epizy.com/4/getEstaciones.php", // https://app.infinityfree.net/accounts
+            // "http://biba8.000webhostapp.com/4/getEstaciones.php", // https://www.000webhost.com/members/website/biba8/build
+            // "http://biba7.000webhostapp.com/4/getEstaciones.php", // also 000webhost
             // not working php ini issues  https://cp1.awardspace.net/beta/
             // not working php ini issues http://palvarez89.5gbfree.com:2082
     };
